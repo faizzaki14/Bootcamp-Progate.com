c36726f2ae1b6a8e2d319e622e26d9434d3db1a0
faizzaki14 <19104075@ittelkom-pwt.ac.id>
2021-12-16 08:05:02 +0700

    Latihan JavaScript 2

---
+++ javascript/JavaScript Study 2/Array/Iterasi dengan Array 1.js
@@ -0,0 +1,6 @@
+// make const fruit and then print it with looping
+// const fruit = ["apple", "banana", "orange", "strawberry"];
+// for (let i = 0; i < fruit.length; i++) {
+//     console.log(fruit[i]);
+// }
+
---
+++ test.js
@@ -0,0 +1,27 @@
+// print number 1-5 using loop for variable number
+// Deklarasikan variable number
+let number = 1;
+//
+// // Tambahkan for loop dibawah
+for (number = 1; number <= 5; number++) {
+  console.log(number);
+}
+//
+// print number 1-5 using loop while variable number
+// Deklarasikan variable number
+// let number = 1;
+//
+// // Tambahkan while loop dibawah
+// while (number <= 5) {
+//   console.log(number);
+//   number++;
+
+
+
+
+let number = 1;
+while (number <= 5) {
+    console.log(number);
+    number++;
+    }
+
